41a162455037c96f2c17158e1aea2be0fe704fb5
Maciej Mionskowski <business@maciekmm.net>
2015-12-02 19:25:01 +0100

    Day 2

---
+++ day-2/README.md
@@ -0,0 +1,9 @@
+Day 2:
+======
+- Language chosen: [lua](http://www.lua.org/)
+- Description: http://pastebin.com/vmjv5J8Y
+- Input: http://pastebin.com/HVxrVu3A
+
+## Tasks:
+
+1./2. Both tasks were pretty simple, I've decided to pack them up into one file.
---
+++ day-2/wrapping.lua
@@ -0,0 +1,32 @@
+local material = 0
+local ribbon = 0
+
+while true do
+  local input = io.read() --read line
+  if input == nil then
+    break
+  end
+
+	local dimensions = {}
+  input:gsub("%d+", function(i) table.insert(dimensions,tonumber(i)) end) --extract raw dimensions
+
+  table.sort(dimensions)
+
+  local areas = {
+		dimensions[1]*dimensions[2],
+		dimensions[2]*dimensions[3],
+		dimensions[1]*dimensions[3]
+	}
+
+  -- areas[1] will the the smallest one
+  material = material+areas[1]+2*(areas[1]+areas[2]+areas[3]) -- calculate total area
+
+  --ribbon for wrapping
+  ribbon = ribbon+2*(dimensions[1]+dimensions[2])
+
+  --bow
+  ribbon = ribbon+dimensions[1]*dimensions[2]*dimensions[3]
+end
+
+print("Material: ", material) --get rid of decimal points
+print("Ribbon: ",ribbon)
